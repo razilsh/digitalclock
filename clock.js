eea3712f67f37cbc51e4da2cf5d372c85bbb7dd4
razilsh <shaikhrazil@gmail.com>
2015-01-04 17:59:11 +0530

    Changed variable names and formatting

--- clock.js
+++ clock.js
@@ -1,31 +1,36 @@
 function displayTime () {
     // get current time
     var current_time = new Date();
-    var weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
-    var dayOfWeek = weekday[current_time.getDay()];
-    var y = current_time.getFullYear();
-    var mo = current_time.getMonth() + 1;
-    var d = current_time.getDate();
-    var h = current_time.getHours();
-    var m = current_time.getMinutes();
-    var s = current_time.getSeconds();
+    var weekday      = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    var dayOfWeek    = weekday[current_time.getDay()];
+    
+    var year     = current_time.getFullYear();
+    var month    = current_time.getMonth() + 1;
+    var date     = current_time.getDate();
+    var hours    = current_time.getHours();
+    var minutes  = current_time.getMinutes();
+    var seconds  = current_time.getSeconds();
+    
     var meridiem = "AM";    
-    if (h > 12) {
-        h = h - 12;
+    
+    if (hours > 12) {
+        hours = hours - 12;
         meridiem = "PM";
     }
 
-   if (h == 0) {
-       h = 12;
+   if (hours == 0) {
+       hours = 12;
    }
 
-   if (s < 10) {
-       s = "0" + s;
+   if (seconds < 10) {
+       seconds = "0" + s;
    }
 
    var clock_div = document.getElementById("clock");
-   clock_div.innerHTML = dayOfWeek + "<br>" + d + "/" + mo + "/" + y + "<br>" + h + ":" + m + ":" + s + " " + meridiem;
+   clock_div.innerHTML = dayOfWeek + "<br>" + date + "/" + month + "/" + year +
+                         "<br>" + hours + ":" + minutes + ":" + seconds + " " + meridiem;
 }
+
 window.onload = function() {
     setInterval(displayTime, 1000);  
 };
